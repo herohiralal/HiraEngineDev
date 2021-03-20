cbb4e186df83d91c18bb2ad2fcf28b8a96e876e6
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-20 07:22:27 +0530

    Created Blackboard testing environment.

---
+++ Assets/BlackboardTester.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Collections.LowLevel.Unsafe;
+
+namespace UnityEngine.Internal
+{
+	public class BlackboardTester : MonoBehaviour
+	{
+		[SerializeField] private HiraBlackboardTemplate template = null;
+		[SerializeField] private HiraBlackboard blackboard = null;
+		[SerializeField] private EffectorTester effect = null;
+
+		private void Awake()
+		{
+			object init = null;
+			template.Initialize(ref init);
+			blackboard.Initialize(ref init);
+		}
+
+		private void OnDestroy()
+		{
+			blackboard.Shutdown();
+			template.Shutdown();
+		}
+
+		private unsafe void OnValidate()
+		{
+			if (effect != null)
+			{
+				var blackboardPtr = (byte*) blackboard.Data.GetUnsafePtr();
+				foreach (var effector in effect.Collection1)
+				{
+					var memory = new byte[effector.MemorySize];
+					fixed (byte* memoryPtr = memory)
+					{
+						effector.AppendMemory(memoryPtr);
+						effector.Function.Invoke(blackboardPtr, memoryPtr);
+					}
+				}
+
+				effect = null;
+			}
+		}
+	}
+}
---
+++ Assets/EffectorTester.cs
@@ -0,0 +1,10 @@
+﻿using HiraEngine.Components.Blackboard.Internal;
+
+namespace UnityEngine.Internal
+{
+	[CreateAssetMenu]
+	public class EffectorTester : HiraCollection<IBlackboardEffector>
+	{
+		
+	}
+}
