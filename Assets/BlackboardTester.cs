7cf8d492fb83a5bcdb69bcb7d3403696e6444398
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 22:29:20 +0530

    Added GOAPDomain for testing.

--- Assets/BlackboardTester.cs
+++ Assets/BlackboardTester.cs
@@ -1,6 +1,4 @@
-﻿using Unity.Collections.LowLevel.Unsafe;
-
-namespace UnityEngine.Internal
+﻿namespace UnityEngine.Internal
 {
 	[System.Serializable, DynamicallyAccessibleEnum("6e8da6b9-998f-4186-81e4-7a9d11fc1ebd"), System.Flags]
 	public enum Class : byte
@@ -12,41 +10,18 @@ namespace UnityEngine.Internal
 	{
 		[SerializeField] private HiraBlackboardTemplate template = null;
 		[SerializeField] private HiraBlackboardComponent blackboard = null;
-		[SerializeField] private HiraSharedBlackboard sharedBlackboard = null;
-		[SerializeField] private EffectorTester effect = null;
 
 		private void Awake()
 		{
 			object init = null;
 			template.Initialize(ref init);
 			blackboard.Initialize(ref init);
-            sharedBlackboard.Initialize(ref init);
         }
 
 		private void OnDestroy()
 		{
-            sharedBlackboard.Shutdown();
 			blackboard.Shutdown();
 			template.Shutdown();
-		}
-
-		private unsafe void OnValidate()
-		{
-			if (effect != null)
-			{
-				var blackboardPtr = (byte*) blackboard.Data.GetUnsafePtr();
-				foreach (var effector in effect.Collection1)
-				{
-					var memory = new byte[effector.MemorySize];
-					fixed (byte* memoryPtr = memory)
-					{
-						effector.AppendMemory(memoryPtr);
-						effector.Function.Invoke(blackboardPtr, memoryPtr);
-					}
-				}
-
-				effect = null;
-			}
 		}
 	}
 }
