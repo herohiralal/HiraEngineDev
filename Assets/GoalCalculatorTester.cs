b0bb63318aa43139dab95b140233e29d48bd7c45
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-23 07:14:03 +0530

    Created a new environment to test goal calculation.

---
+++ Assets/GoalCalculatorTester.cs
@@ -0,0 +1,38 @@
+﻿using HiraEngine.Components.AI.LGOAP;
+using HiraEngine.Components.AI.LGOAP.Internal;
+using Unity.Collections;
+
+namespace UnityEngine.Internal
+{
+	public class GoalCalculatorTester : MonoBehaviour
+	{
+		[SerializeField] private HiraBlackboardComponent blackboard = null;
+		[SerializeField] private GoalOrientedActionPlannerDomain domain = null;
+		[HiraButton(nameof(UpdateGoal))]
+		[SerializeField] private Stub updateGoal = default;
+		[SerializeField] private Goal goal = null;
+
+		private FlipFlopPool<PlannerResult> _result;
+
+		private void Awake()
+		{
+			_result.First = new PlannerResult(1, Allocator.Persistent);
+			_result.Second = new PlannerResult(1, Allocator.Persistent);
+		}
+
+		private void OnDestroy()
+		{
+			_result.First.Dispose();
+			_result.Second.Dispose();
+			_result = default;
+		}
+
+		public void UpdateGoal()
+		{
+			var jobHandle = domain.ScheduleGoalCalculatorJob(blackboard, _result.First[0], _result.Second);
+			_result.Flip();
+			jobHandle.Complete();
+			goal = domain.Collection1[_result.First[0]];
+		}
+	}
+}
--- Assets/Initializer.cs
+++ Assets/Initializer.cs
@@ -1,4 +1,6 @@
-﻿namespace UnityEngine.Internal
+﻿using HiraEngine.Components.AI.LGOAP;
+
+namespace UnityEngine.Internal
 {
 	[System.Serializable, DynamicallyAccessibleEnum("6e8da6b9-998f-4186-81e4-7a9d11fc1ebd"), System.Flags]
 	public enum Class : byte
@@ -6,20 +8,23 @@
 		None = 0, Warrior = 1 << 0, Mage = 1 << 1, Rogue = 1 << 2
 	}
 
-	public class BlackboardTester : MonoBehaviour
+	public class Initializer : MonoBehaviour
 	{
 		[SerializeField] private HiraBlackboardTemplate template = null;
 		[SerializeField] private HiraBlackboardComponent blackboard = null;
+		[SerializeField] private GoalOrientedActionPlannerDomain domain = null;
 
 		private void Awake()
 		{
 			object init = null;
 			template.Initialize(ref init);
 			blackboard.Initialize(ref init);
-        }
+			domain.Initialize(ref init);
+		}
 
 		private void OnDestroy()
 		{
+			domain.Shutdown();
 			blackboard.Shutdown();
 			template.Shutdown();
 		}
