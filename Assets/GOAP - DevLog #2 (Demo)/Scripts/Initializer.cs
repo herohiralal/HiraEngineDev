e28c2fbfc755c82c289964dcb307f5a13e30c0a4
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-27 08:21:34 +0530

    Created: patroller test environment.

--- Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
@@ -1,4 +1,5 @@
-﻿using HiraEngine.Components.AI.LGOAP;
+﻿using System.Collections.Generic;
+using HiraEngine.Components.AI.LGOAP;
 
 namespace UnityEngine.Internal
 {
@@ -11,22 +12,44 @@ namespace UnityEngine.Internal
 	public class Initializer : MonoBehaviour
 	{
 		[SerializeField] private HiraBlackboardTemplate template = null;
-		[SerializeField] private HiraBlackboardComponent blackboard = null;
 		[SerializeField] private GoalOrientedActionPlannerDomain domain = null;
+
+		[SerializeField] private GameObject agentPrefab = null;
+		private readonly Stack<IInitializable> _initializedObjects = new Stack<IInitializable>();
 
 		private void Awake()
 		{
 			object init = null;
+
 			template.Initialize(ref init);
-			blackboard.Initialize(ref init);
+			_initializedObjects.Push(template);
+
 			domain.Initialize(ref init);
+			_initializedObjects.Push(domain);
+			
+			var agent = Instantiate(agentPrefab, transform.position, Quaternion.identity, null);
+
+			var blackboard = agent.GetComponent<HiraBlackboard>();
+			if (blackboard != null && blackboard is IInitializable initializableBlackboard)
+			{
+				initializableBlackboard.Initialize(ref init);
+				_initializedObjects.Push(initializableBlackboard);
+			}
+
+			var planner = agent.GetComponent<GoalOrientedActionPlanner>();
+			if (planner != null)
+			{
+				planner.Initialize(ref init);
+				_initializedObjects.Push(planner);
+			}
 		}
 
 		private void OnDestroy()
 		{
-			domain.Shutdown();
-			blackboard.Shutdown();
-			template.Shutdown();
+			while (_initializedObjects.Count > 0)
+			{
+				_initializedObjects.Pop().Shutdown();
+			}
 		}
 	}
 }
