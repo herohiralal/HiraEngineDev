b29228eababed17d0d5edb11d6452efaec3ac7c9
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-27 10:19:42 +0530

    Matched Initializer with new IInitializable interface.

--- Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
@@ -3,13 +3,7 @@ using HiraEngine.Components.AI.LGOAP;
 
 namespace UnityEngine.Internal
 {
-	[System.Serializable, DynamicallyAccessibleEnum("6e8da6b9-998f-4186-81e4-7a9d11fc1ebd"), System.Flags]
-	public enum Class : byte
-	{
-		None = 0, Warrior = 1 << 0, Mage = 1 << 1, Rogue = 1 << 2
-	}
-
-	public class Initializer : MonoBehaviour
+    public class Initializer : MonoBehaviour
 	{
 		[SerializeField] private HiraBlackboardTemplate template = null;
 		[SerializeField] private GoalOrientedActionPlannerDomain domain = null;
@@ -19,12 +13,10 @@ namespace UnityEngine.Internal
 
 		private void Awake()
 		{
-			object init = null;
-
-			template.Initialize(ref init);
+            template.Initialize();
 			_initializedObjects.Push(template);
 
-			domain.Initialize(ref init);
+			domain.Initialize();
 			_initializedObjects.Push(domain);
 			
 			var agent = Instantiate(agentPrefab, transform.position, Quaternion.identity, null);
@@ -32,14 +24,14 @@ namespace UnityEngine.Internal
 			var blackboard = agent.GetComponent<HiraBlackboard>();
 			if (blackboard != null && blackboard is IInitializable initializableBlackboard)
 			{
-				initializableBlackboard.Initialize(ref init);
+				initializableBlackboard.Initialize();
 				_initializedObjects.Push(initializableBlackboard);
 			}
 
 			var planner = agent.GetComponent<GoalOrientedActionPlanner>();
 			if (planner != null)
 			{
-				planner.Initialize(ref init);
+				planner.Initialize();
 				_initializedObjects.Push(planner);
 			}
 		}
