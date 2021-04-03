73c9df9bb19ca7b81539f90fe4e803e130bea758
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-04-03 07:15:32 +0530

    Set up LGOAP agent test environment.

--- Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/Initializer.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using HiraEngine.Components.AI.LGOAP;
+using UnityEngine.SceneManagement;
 
 namespace UnityEngine.Internal
 {
@@ -11,14 +13,35 @@ namespace UnityEngine.Internal
 		[SerializeField] private GameObject agentPrefab = null;
 		private readonly Stack<IInitializable> _initializedObjects = new Stack<IInitializable>();
 
+		[SerializeField] private StringReference sceneName = null;
+
+		[SerializeField] private GameObject mainMenu = null;
+		[SerializeField] private GameObject loader = null;
+		[SerializeField] private GameObject gameScreen = null;
+
 		private void Awake()
 		{
+			loader.SetActive(false);
+			gameScreen.SetActive(false);
+			mainMenu.SetActive(true);
+			
             template.Initialize();
-			_initializedObjects.Push(template);
 
 			domain.Initialize();
-			_initializedObjects.Push(domain);
+		}
+
+		public void Begin() => StartCoroutine(BeginCoroutine());
+
+		private IEnumerator BeginCoroutine()
+		{
+			loader.SetActive(true);
+			mainMenu.SetActive(false);
 			
+			var sceneLoadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+			while (!sceneLoadOperation.isDone) yield return null;
+
+			SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+
 			var agent = Instantiate(agentPrefab, transform.position, Quaternion.identity, null);
 
 			var blackboard = agent.GetComponent<HiraBlackboard>();
@@ -26,6 +49,8 @@ namespace UnityEngine.Internal
 			{
 				initializableBlackboard.Initialize();
 				_initializedObjects.Push(initializableBlackboard);
+				while (initializableBlackboard.InitializationStatus != InitializationState.Active)
+					yield return null;
 			}
 
 			var planner = agent.GetComponent<GoalOrientedActionPlanner>();
@@ -33,15 +58,50 @@ namespace UnityEngine.Internal
 			{
 				planner.Initialize();
 				_initializedObjects.Push(planner);
+				while (planner.InitializationStatus != InitializationState.Active)
+					yield return null;
 			}
+
+			var layeredPlanner = agent.GetComponent<LayeredGoalOrientedActionPlanner>();
+			if (layeredPlanner != null)
+			{
+				layeredPlanner.Initialize();
+				_initializedObjects.Push(layeredPlanner);
+				while (layeredPlanner.InitializationStatus != InitializationState.Active)
+					yield return null;
+			}
+
+			gameScreen.SetActive(true);
+			loader.SetActive(false);
+		}
+
+		public void End() => StartCoroutine(EndCoroutine());
+
+		private IEnumerator EndCoroutine()
+		{
+			loader.SetActive(true);
+			gameScreen.SetActive(false);
+
+			while (_initializedObjects.Count > 0)
+			{
+				var current = _initializedObjects.Pop();
+				current.Shutdown();
+				while (current.InitializationStatus != InitializationState.Inactive)
+					yield return null;
+			}
+
+			var sceneUnloadOp = SceneManager.UnloadSceneAsync(sceneName);
+			while (!sceneUnloadOp.isDone) yield return null;
+
+			mainMenu.SetActive(true);
+			loader.SetActive(false);
 		}
 
 		private void OnDestroy()
 		{
-			while (_initializedObjects.Count > 0)
-			{
-				_initializedObjects.Pop().Shutdown();
-			}
+			domain.Shutdown();
+			
+			template.Shutdown();
 		}
 	}
 }
---
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/MultiLayeredAgentComponentContainer.cs
@@ -0,0 +1,18 @@
+﻿using HiraEngine.Components.AI.LGOAP;
+using UnityEngine.AI;
+
+namespace UnityEngine.Internal
+{
+	public class MultiLayeredAgentComponentContainer : HiraComponentContainer, IContainsComponent<NavMeshAgent>, IContainsComponent<IBlackboardComponent>, IContainsComponent<LayeredGoalOrientedActionPlanner>
+	{
+		[SerializeField] private NavMeshAgent navMeshAgent;
+		[SerializeField] private HiraBlackboard blackboard;
+		[SerializeField] private LayeredGoalOrientedActionPlanner planner;
+
+		NavMeshAgent IContainsComponent<NavMeshAgent>.Component => navMeshAgent;
+
+		IBlackboardComponent IContainsComponent<IBlackboardComponent>.Component => blackboard;
+
+		LayeredGoalOrientedActionPlanner IContainsComponent<LayeredGoalOrientedActionPlanner>.Component => planner;
+	}
+}
