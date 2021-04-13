7598dee5e65be45dbdf94fbd2379423068100cfb
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-04-13 17:16:22 +0530

    Messed around with a console.

---
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/HiraConsoleController.cs
@@ -0,0 +1,38 @@
+﻿namespace UnityEngine.Internal
+{
+    [HiraManager]
+    public class HiraConsoleController : MonoBehaviour
+    {
+        private bool _consoleActive = false;
+        [SerializeField] private HiraConsoleGUI gui = null;
+
+        private void Awake()
+        {
+            gui = gameObject.AddComponent<HiraConsoleGUI>();
+            gui.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            gui.enabled = true;
+            Destroy(gui);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.BackQuote)) Toggle();
+        }
+
+        public void Toggle()
+        {
+            _consoleActive = !_consoleActive;
+            Time.timeScale = _consoleActive ? 0f : 1f;
+            gui.enabled = _consoleActive;
+        }
+
+        public void Handle(string command)
+        {
+            
+        }
+    }
+}
---
+++ Assets/GOAP - DevLog #2 (Demo)/Scripts/HiraConsoleGUI.cs
@@ -0,0 +1,46 @@
+﻿namespace UnityEngine.Internal
+{
+    public class HiraConsoleGUI : MonoBehaviour
+    {
+        [SerializeField] private string input = "";
+
+        [SerializeField] private HiraConsoleController controller = null;
+
+        private void Awake()
+        {
+            controller = GetComponent<HiraConsoleController>();
+        }
+
+        private void OnDestroy()
+        {
+            controller = null;
+        }
+
+        private void OnEnable()
+        {
+            input = "";
+        }
+
+        private void OnGUI()
+        {
+            var y = 0f;
+
+            GUI.Box(new Rect(0, y, Screen.width, 40), "");
+
+            const string controlName = "Console";
+
+            GUI.SetNextControlName(controlName);
+            input = GUI.TextArea(new Rect(10f, y + 5f, Screen.width - 20f, 20f), input);
+            GUI.FocusControl(controlName);
+
+            if (input.EndsWith("`"))
+            {
+                controller.Toggle();
+            }
+            else if (input.EndsWith("\n"))
+            {
+                controller.Toggle();
+            }
+        }
+    }
+}
