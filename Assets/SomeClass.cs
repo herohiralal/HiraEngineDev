3861fcb25fe182a01521a48af62b9800ad48ed5e
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-04-14 12:27:55 +0530

    Added: Console test environment.

---
+++ Assets/SomeClass.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	[HiraConsole]
+	public static class SomeClass
+	{
+		[HiraConsole, UsedImplicitly]
+		private static void Add(float a, float b) => Debug.Log(a + b);
+
+		[HiraConsole, UsedImplicitly]
+		private static void Subtract(float a, float b) => Debug.Log(a - b);
+
+		[HiraConsole, UsedImplicitly]
+		private static void Multiply(float a, float b) => Debug.Log(a * b);
+
+		[HiraConsole, UsedImplicitly]
+		private static void Divide(float a, float b) => Debug.Log(a / b);
+
+		[HiraConsole, UsedImplicitly]
+		private static void Power(float a, float b) => Debug.Log(Mathf.Pow(a, b));
+	}
+}
