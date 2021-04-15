885e5143a999c15444612610efaf94ee1996b1b9
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-04-15 12:49:59 +0530

    Added: HiraConsole custom naming test environment.

--- Assets/SomeClass.cs
+++ Assets/SomeClass.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-	[HiraConsole]
+	[HiraConsoleType("Mathematics")]
 	public static class SomeClass
 	{
-		[HiraConsole, UsedImplicitly]
+		[HiraConsoleCallable, UsedImplicitly]
 		private static void Add(float a, float b) => Debug.Log(a + b);
 
-		[HiraConsole, UsedImplicitly]
+		[HiraConsoleCallable, UsedImplicitly]
 		private static void Subtract(float a, float b) => Debug.Log(a - b);
 
-		[HiraConsole, UsedImplicitly]
+		[HiraConsoleCallable, UsedImplicitly]
 		private static void Multiply(float a, float b) => Debug.Log(a * b);
 
-		[HiraConsole, UsedImplicitly]
+		[HiraConsoleCallable, UsedImplicitly]
 		private static void Divide(float a, float b) => Debug.Log(a / b);
 
-		[HiraConsole, UsedImplicitly]
+		[HiraConsoleCallable, UsedImplicitly]
 		private static void Power(float a, float b) => Debug.Log(Mathf.Pow(a, b));
 	}
 }
